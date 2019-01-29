9f06dcae432d7109decc7140273dc0bd1a74f267
lovesh harchandani <lovesh.bond@gmail.com>
2019-01-29 19:50:43 +0530

    remove/comment redundant code
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- src/r1cs/mod.rs
+++ src/r1cs/mod.rs
@@ -10,6 +10,7 @@ mod prover;
 mod verifier;
 pub mod value;
 pub mod gadget_range_proof;
+pub mod gadget_set_membership;
 
 pub use self::constraint_system::{
     ConstraintSystem, RandomizableConstraintSystem, RandomizedConstraintSystem,
